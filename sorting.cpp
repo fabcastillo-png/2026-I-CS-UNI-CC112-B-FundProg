1ced902887d5b57ddad6dccd6ddb88a1eaec3d56
Fabio <tuemail@gmail.com>
2026-04-07 21:40:15 -0500

    tarea

--- sorting.cpp
+++ sorting.cpp
@@ -1,196 +1,194 @@
-#include "sorting.h"
-#include "util.h"
-#include <iostream>
-// #include "array.h"
+    #include "sorting.h"
+    #include "util.h"
+    #include <iostream>
+    //#include "array.h"
 
-using namespace std;
+    using namespace std;
 
-void DemoBurbuja() {
-  cout << "DemoBurbuja \n";
+    void DemoBurbuja() {
+    cout << "DemoBurbuja \n";
 
-  cout << "BurbujaClasico \n";
-  T1 arr1[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
-  auto n1 = sizeof(arr1) / sizeof(arr1[0]);
+    cout << "BurbujaClasico \n";
+    T1 arr1[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
+    auto n1 = sizeof(arr1) / sizeof(arr1[0]);
 
-  PrintArray(arr1, n1);
-  BurbujaClasico(arr1, n1, Mayor<T1>);
-  PrintArray(arr1, n1);
-  BurbujaClasico(arr1, n1, Menor<T1>);
-  PrintArray(arr1, n1);
+    PrintArray(arr1, n1);
+    BurbujaClasico(arr1, n1, Mayor<T1>);
+    PrintArray(arr1, n1);
+    BurbujaClasico(arr1, n1, Menor<T1>);
+    PrintArray(arr1, n1);
 
-  cout << "BurbujaRecursivo \n";
-  T1 arr2[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
-  auto n2 = sizeof(arr2) / sizeof(arr2[0]);
+    cout << "BurbujaRecursivo \n";
+    T1 arr2[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
+    auto n2 = sizeof(arr2) / sizeof(arr2[0]);
 
-  PrintArray(arr2, n2);
-  BurbujaRecursivo(arr2, n2, Mayor<T1>);
-  PrintArray(arr2, n2);
-  BurbujaRecursivo(arr2, n2, Menor<T1>);
-  PrintArray(arr2, n2);
+    PrintArray(arr2, n2);
+    BurbujaRecursivo(arr2, n2, Mayor<T1>);
+    PrintArray(arr2, n2);
+    BurbujaRecursivo(arr2, n2, Menor<T1>);
+    PrintArray(arr2, n2);
 
-  T2 arr3[] = {5.2, 7.5, 8.1, 1.9, 8.9, 9.1, 4.4, 7.7, 3.3, 6.6};
-  auto n3 = sizeof(arr3) / sizeof(arr3[0]);
-  BurbujaRecursivo(arr3, n3, Mayor<T2>);
-  cout << "Array ordenado Ascendente:\n";
-  PrintArray(arr3, n3);
-  BurbujaRecursivo(arr3, n3, Menor<T2>);
-  cout << "Array ordenado Descendente:\n";
-  PrintArray(arr3, n3);
+    T2 arr3[] = {5.2, 7.5, 8.1, 1.9, 8.9, 9.1, 4.4, 7.7, 3.3, 6.6};
+    auto n3 = sizeof(arr3) / sizeof(arr3[0]);
+    BurbujaRecursivo(arr3, n3, Mayor<T2>);
+    cout << "Array ordenado Ascendente:\n";
+    PrintArray(arr3, n3);
+    BurbujaRecursivo(arr3, n3, Menor<T2>);
+    cout << "Array ordenado Descendente:\n";
+    PrintArray(arr3, n3);
 
-  string arr4[] = {"Hola", "Mundo", "Adios", "Mundo"};
-  auto n4 = sizeof(arr4) / sizeof(arr4[0]);
-  BurbujaRecursivo(arr4, n4, Mayor<string>);
-  cout << "Array ordenado Ascendente:\n";
-  PrintArray(arr4, n4);
-  BurbujaRecursivo(arr4, n4, &Menor<string>);
-  cout << "Array ordenado Descendente:\n";
-  PrintArray(arr4, n4);
+    string arr4[] = {"Hola", "Mundo", "Adios", "Mundo"};
+    auto n4 = sizeof(arr4) / sizeof(arr4[0]);
+    BurbujaRecursivo(arr4, n4, Mayor<string>);
+    cout << "Array ordenado Ascendente:\n";
+    PrintArray(arr4, n4);
+    BurbujaRecursivo(arr4, n4, &Menor<string>);
+    cout << "Array ordenado Descendente:\n";
+    PrintArray(arr4, n4);
 
-  // BurbujaRecursivo(arr, n, &Menor);
-  // cout << "Array ordenado Descendente:\n";
-  // PrintArray(arr, 10, cout);
+    // BurbujaRecursivo(arr, n, &Menor);
+    // cout << "Array ordenado Descendente:\n";
+    // PrintArray(arr, 10, cout);
 
-  // cout << endl;
-  int a = 3;
-  double b = 3.14;
-  auto x = suma(a, b);
-  cout << x << endl;
+    // cout << endl;
+    int a = 3;
+    double b = 3.14;
+    auto x = suma(a, b);
+    cout << x << endl;
 
-  string str1 = "Fund", str2 = "Prog";
-  auto str3 = suma(str1, str2);
-  cout << str3 << endl;
+    string str1 = "Fund", str2 = "Prog";
+    auto str3 = suma(str1, str2);
+    cout << str3 << endl;
 
-  //     string str3 = "CS-UNI";
-  //     auto str4 = suma(str3, 7.7);
-  //     cout << str4 << endl;
-}
+    //     string str3 = "CS-UNI";
+    //     auto str4 = suma(str3, 7.7);
+    //     cout << str4 << endl;
+    }
 
-// ContainerRange particionar(T1* arr, ContainerRange first, ContainerRange
-// last, CompFunc pComp) {
-//     auto pivote = arr[last];  // Pivote es el elemento de referencia
-//     auto i = (first - 1);
+    // ContainerRange particionar(T1* arr, ContainerRange first, ContainerRange
+    // last, CompFunc pComp) {
+    //     auto pivote = arr[last];  // Pivote es el elemento de referencia
+    //     auto i = (first - 1);
 
-//     for (auto j = first; j <= last - 1; j++) {
-//         if (arr[j] == pivote) ++i;
-//         if ( (*pComp)(arr[j], pivote) ){
-//             ++i; intercambiar(arr[i], arr[j]);
-//         }
-//     }
-//     intercambiar(arr[i + 1], arr[last]);
-//     return (i + 1);
-// }
+    //     for (auto j = first; j <= last - 1; j++) {
+    //         if (arr[j] == pivote) ++i;
+    //         if ( (*pComp)(arr[j], pivote) ){
+    //             ++i; intercambiar(arr[i], arr[j]);
+    //         }
+    //     }
+    //     intercambiar(arr[i + 1], arr[last]);
+    //     return (i + 1);
+    // }
 
-// void QuickSort(T1* arr, ContainerRange first, ContainerRange last, CompFunc
-// pComp) {
-//     if (first < last) {
-//         auto pi = particionar(arr, first, last, pComp);
-//         QuickSort(arr, first, pi - 1, pComp);
-//         QuickSort(arr, pi + 1, last, pComp);
-//     }
-// }
+    // void QuickSort(T1* arr, ContainerRange first, ContainerRange last, CompFunc
+    // pComp) {
+    //     if (first < last) {
+    //         auto pi = particionar(arr, first, last, pComp);
+    //         QuickSort(arr, first, pi - 1, pComp);
+    //         QuickSort(arr, pi + 1, last, pComp);
+    //     }
+    // }
 
-void DemoQuickSort() {
-    cout << "DemoQuickSort \n";
-    T1 arr[] = {5, 2, 8, 15, 1, 9, 4, 7, 3, 6};
-    auto n = sizeof(arr) / sizeof(arr[0]);
+    void DemoQuickSort() {
+        cout << "DemoQuickSort \n";
+        int arr[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
+        auto n = sizeof(arr) / sizeof(arr[0]);
+        QuickSort(arr, 0, n - 1, &Mayor<int>);
+        cout << "Arreglo ordenado Ascendente: \n";
+        PrintArray(arr, n);
+            QuickSort(arr, 0, n - 1, &Menor<int>);
+    cout << "Arreglo ordenado Descendente: \n";
+        PrintArray(arr, n);
 
-//     QuickSort(arr, 0, n - 1, &Mayor);
-//     cout << "Arreglo ordenado Ascendente: \n";
-//     PrintArray(arr, n, cout);
+        cout << endl;
+    }
 
-//     QuickSort(arr, 0, n - 1, &Menor);
-//     cout << "Arreglo ordenado Descendente: \n";
-//     PrintArray(arr, n, cout);
+    // // Función para mezclar dos subarreglos ordenados de arr[].
+    // void Merge(T1* arr, const ContainerRange left,
+    //                                    const ContainerRange mid,
+    //                                    const ContainerRange right,
+    //                                    CompFunc pComp) {
+    //     auto const subArrayOne = mid - left + 1;
+    //     auto const subArrayTwo = right - mid;
 
-//     cout << endl;
-}
+    //     // Crear arrays temporales
+    //     auto *leftArray = new T1[subArrayOne],
+    //          *rightArray = new T1[subArrayTwo];
 
-// // Función para mezclar dos subarreglos ordenados de arr[].
-// void Merge(T1* arr, const ContainerRange left,
-//                                    const ContainerRange mid,
-//                                    const ContainerRange right,
-//                                    CompFunc pComp) {
-//     auto const subArrayOne = mid - left + 1;
-//     auto const subArrayTwo = right - mid;
+    //     // Copiar datos a los arrays temporales leftArray[] y rightArray[]
+    //     for (auto i = 0; i < subArrayOne; i++)
+    //         leftArray[i] = arr[left + i];
+    //     for (auto j = 0; j < subArrayTwo; j++)
+    //         rightArray[j] = arr[mid + 1 + j];
 
-//     // Crear arrays temporales
-//     auto *leftArray = new T1[subArrayOne],
-//          *rightArray = new T1[subArrayTwo];
+    //     auto indexOfSubArrayOne = 0, // Índice inicial del primer sub-array
+    //         indexOfSubArrayTwo = 0; // Índice inicial del segundo sub-array
+    //     ContainerRange indexOfMergedArray = left; // Índice inicial del array
+    //     mezclado
 
-//     // Copiar datos a los arrays temporales leftArray[] y rightArray[]
-//     for (auto i = 0; i < subArrayOne; i++)
-//         leftArray[i] = arr[left + i];
-//     for (auto j = 0; j < subArrayTwo; j++)
-//         rightArray[j] = arr[mid + 1 + j];
+    //     // Mezclar los arrays temporales de vuelta a arr[left..right]
+    //     while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo <
+    //     subArrayTwo) {
+    //         if ( (*pComp)(rightArray[indexOfSubArrayTwo],
+    //         leftArray[indexOfSubArrayOne])  ) {
+    //             arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+    //             indexOfSubArrayOne++;
+    //         } else {
+    //             arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+    //             indexOfSubArrayTwo++;
+    //         }
+    //         indexOfMergedArray++;
+    //     }
+    //     // Copiar los elementos restantes de left[], si los hay
+    //     while (indexOfSubArrayOne < subArrayOne) {
+    //         arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+    //         indexOfSubArrayOne++;
+    //         indexOfMergedArray++;
+    //     }
+    //     // Copiar los elementos restantes de right[], si los hay
+    //     while (indexOfSubArrayTwo < subArrayTwo) {
+    //         arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+    //         indexOfSubArrayTwo++;
+    //         indexOfMergedArray++;
+    //     }
 
-//     auto indexOfSubArrayOne = 0, // Índice inicial del primer sub-array
-//         indexOfSubArrayTwo = 0; // Índice inicial del segundo sub-array
-//     ContainerRange indexOfMergedArray = left; // Índice inicial del array
-//     mezclado
+    //     delete[] leftArray;
+    //     delete[] rightArray;
+    // }
 
-//     // Mezclar los arrays temporales de vuelta a arr[left..right]
-//     while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo <
-//     subArrayTwo) {
-//         if ( (*pComp)(rightArray[indexOfSubArrayTwo],
-//         leftArray[indexOfSubArrayOne])  ) {
-//             arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
-//             indexOfSubArrayOne++;
-//         } else {
-//             arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
-//             indexOfSubArrayTwo++;
-//         }
-//         indexOfMergedArray++;
-//     }
-//     // Copiar los elementos restantes de left[], si los hay
-//     while (indexOfSubArrayOne < subArrayOne) {
-//         arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
-//         indexOfSubArrayOne++;
-//         indexOfMergedArray++;
-//     }
-//     // Copiar los elementos restantes de right[], si los hay
-//     while (indexOfSubArrayTwo < subArrayTwo) {
-//         arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
-//         indexOfSubArrayTwo++;
-//         indexOfMergedArray++;
-//     }
+    // // left es para el índice izquierdo y right es para el índice derecho del
+    // // sub-array de arr a ordenar
+    // void MergeSort( T1* arr,
+    //                 ContainerRange const begin,
+    //                 ContainerRange const end,
+    //                 CompFunc pComp) {
+    //     if (begin >= end)
+    //         return; // Return recursivamente
 
-//     delete[] leftArray;
-//     delete[] rightArray;
-// }
+    //     auto mid = begin + (end - begin) / 2;
+    //     MergeSort(arr, begin, mid, pComp);
+    //     MergeSort(arr, mid + 1, end, pComp);
+    //     Merge(arr, begin, mid, end, pComp);
+    // }
 
-// // left es para el índice izquierdo y right es para el índice derecho del
-// // sub-array de arr a ordenar
-// void MergeSort( T1* arr,
-//                 ContainerRange const begin,
-//                 ContainerRange const end,
-//                 CompFunc pComp) {
-//     if (begin >= end)
-//         return; // Return recursivamente
+    void DemoMergeSort(){
+        cout << "DemoMergeSort" << endl;
+        int arr[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
+        auto n = sizeof(arr) / sizeof(arr[0]);
 
-//     auto mid = begin + (end - begin) / 2;
-//     MergeSort(arr, begin, mid, pComp);
-//     MergeSort(arr, mid + 1, end, pComp);
-//     Merge(arr, begin, mid, end, pComp);
-// }
+    MergeSort(arr, 0, n-1, &Mayor<int>);
+        cout << "Array ordenado Ascendente:\n";
+            PrintArray(arr, n);
 
-void DemoMergeSort(){
-    cout << "DemoMergeSort" << endl;
-    T1 arr[] = {5, 2, 8, 1, 15, 9, 4, 7, 3, 6};
-    auto n = sizeof(arr) / sizeof(arr[0]);
+        MergeSort(arr, 0, n-1, &Menor<int>);
+        cout << "Array ordenado Descendente:\n";
+        PrintArray(arr, n);
 
-//     MergeSort(arr, 0, n-1, &Mayor);
-//     cout << "Array ordenado Ascendente:\n";
-//     PrintArray(arr, n, cout);
+        cout << endl;
+    }
 
-//     MergeSort(arr, 0, n-1, &Menor);
-//     cout << "Array ordenado Descendente:\n";
-//     PrintArray(arr, n, cout);
-
-    cout << endl;
-}
-
-void DemoSorting() {
-    DemoBurbuja();
-    DemoQuickSort();
-    DemoMergeSort();
-}
+    void DemoSorting() {
+        DemoBurbuja(); 
+        DemoQuickSort();
+        DemoMergeSort();
+    }
--- sorting.h
+++ sorting.h
@@ -2,7 +2,12 @@
 #define __SORTING_H__
 #include <iostream>
 #include "types.h"
-// #include "compareFunc.h"
+#include <string>
+ void DemoMergeSort();
+
+void DemoSorting() ;
+
+//#endif // __SORTING_H__// #include "compareFunc.h"
 using namespace std;
 using ContainerRange    = int;
 
@@ -55,14 +60,62 @@ void BurbujaRecursivo(T arr[], ContainerRange n, Func func) {
 }
 void DemoBurbuja();
 
-// ContainerRange  particionar(ContainerElemType1* arr, ContainerRange first, ContainerRange last, CompFunc pComp);
-// void QuickSort  (ContainerElemType1* arr, ContainerRange first, ContainerRange last, CompFunc pComp);
-// void DemoQuickSort();
 
+// ContainerRange  particionar(ContainerElemType1* arr, ContainerRange first, ContainerRange last, CompFunc pComp);
+// void QuickSort  (ContainerElemType1* arr, ContainerRange first, ContainerRange last, CompFunc pComp);  
+
+//void DemonQuickStor();
+// --- MERGE SORT ---
+template <typename T, typename Func>
+void merge(T arr[], ContainerRange l, ContainerRange m, ContainerRange r, Func func) {
+    ContainerRange n1 = m - l + 1;
+    ContainerRange n2 = r - m;
+    T* L = new T[n1];
+    T* R = new T[n2];
+
+    for (ContainerRange i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (ContainerRange j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    ContainerRange i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (func(L[i], R[j])) { arr[k] = L[i]; i++; }
+        else { arr[k] = R[j]; j++; }
+        k++;
+    }
+    while (i < n1) { arr[k] = L[i]; i++; k++; }
+    while (j < n2) { arr[k] = R[j]; j++; k++; }
+    delete[] L; delete[] R;
+}
+
+template <typename T, typename Func>
+void MergeSort(T arr[], ContainerRange l, ContainerRange r, Func func) {
+    if (l < r) {
+        ContainerRange m = l + (r - l) / 2;
+        MergeSort(arr, l, m, func);
+        MergeSort(arr, m + 1, r, func);
+        merge(arr, l, m, r, func);
+    }
+}
+
+// --- QUICK SORT ---
+template <typename T, typename Func>
+void QuickSort(T arr[], ContainerRange low, ContainerRange high, Func func) {
+    if (low < high) {
+        T pivot = arr[high];
+        ContainerRange i = (low - 1);
+        for (ContainerRange j = low; j <= high - 1; j++) {
+            if (func(arr[j], pivot)) {
+                i++;
+                intercambiar(arr[i], arr[j]);
+            }
+        }
+        intercambiar(arr[i + 1], arr[high]);
+        ContainerRange pi = i + 1;
+        QuickSort(arr, low, pi - 1, func);
+        QuickSort(arr, pi + 1, high, func);
+    }
+}
+#endif
 // void Merge(ContainerElemType1* arr, const ContainerRange left, const ContainerRange mid, const ContainerRange right, CompFunc pComp); 
 // void MergeSort(ContainerElemType1* arr, const ContainerRange begin, const ContainerRange end, CompFunc pComp);
-// void DemoMergeSort();
-
-void DemoSorting();
-
-#endif // __SORTING_H__
+//
